c2dcc0294059981ae6954242d9531f1de3d0a8e7
Asif <asifkn1219@gmail.com>
2025-08-14 16:48:33 +0600

    Title Component

---
+++ src/Page/Title.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useRef } from "react";
+import type { NodeData } from "../utils/types";
+import styles from "./Title.module.css";
+
+type TitleProps = {
+  title: string;
+  changePageTitle: (title: string) => void;
+  addNode: (node: NodeData, index: number) => void;
+};
+
+export const Title = ({ title, changePageTitle, addNode }: TitleProps) => {
+  const headerRef = useRef<HTMLHeadingElement>(null);
+
+  useEffect(() => {
+    const isFocused = document.activeElement === headerRef.current;
+    if (!isFocused && headerRef.current) {
+      headerRef.current.textContent = title;
+    }
+  }, [title]);
+
+  return (
+    <div className={styles.container}>
+      <h1
+        className={styles.title}
+        contentEditable
+        suppressContentEditableWarning
+        ref={headerRef}
+        onInput={(event) => {
+          changePageTitle(event.currentTarget.textContent || "");
+        }}
+        onKeyDown={(event) => {
+          if (event.key === "Enter") {
+            event.preventDefault();
+            addNode({ id: crypto.randomUUID(), type: "text", value: "" }, 0);
+            changePageTitle(event.currentTarget.textContent || "");
+          }
+        }}
+      />
+    </div>
+  );
+};
