28352c7c55f9f1141cede34b8facfc9ee85bc48e
Asif <asifkn1219@gmail.com>
2025-08-14 16:45:21 +0600

    Cover Image Component

---
+++ src/Page/Cover.tsx
@@ -0,0 +1,33 @@
+// import { useRef } from "react";
+import styles from "./Cover.module.css";
+// import { FileImage } from "../components/FileImage";
+// import { uploadImage } from "../utils/uploadImage";
+
+export const Cover = () => {
+  // const fileInputRef = useRef<HTMLInputElement>(null);
+
+  // const onChangeCover = () => {
+  //   fileInputRef.current?.click();
+  // };
+
+  // const onChangeCoverUpload = async (
+  //   event: React.ChangeEvent<HTMLInputElement>
+  // ) => {
+  //   const target = event.target;
+  // };
+
+  return (
+    <div className={styles.cover}>
+      <img src="/ztm-notes.png" alt="Cover" className={styles.image} />
+      {/* <button className={styles.button} onClick={onChangeCover}>
+        Change Cover
+      </button> */}
+      {/* <input
+        onChange={onChangeCoverUpload}
+        style={{ display: "none" }}
+        ref={fileInputRef}
+        type="file"
+      /> */}
+    </div>
+  );
+};
